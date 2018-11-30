d81954ef78396a12841088ea98f23e33da1c5d9a
Ddow <44941588+AndrewDdow@users.noreply.github.com>
2018-11-30 11:52:33 +0300

    00001

--- src/pkg30/pkg11/pkg18/Facet.java
+++ src/pkg30/pkg11/pkg18/Facet.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg30.pkg11.pkg18;
 
-/**
- *
- * @author andre
- */
 public class Facet {
-    
+
 }
