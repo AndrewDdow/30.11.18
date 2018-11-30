8dc7ef2db776e5f7655538fbf9612614e320fb1e
Ddow <44941588+AndrewDdow@users.noreply.github.com>
2018-11-30 11:48:02 +0300

    Initial commit

---
+++ src/pkg30/pkg11/pkg18/Main.java
@@ -0,0 +1,48 @@
+package pkg30.pkg11.pkg18;
+
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+//        Vector a = new Vector(0, 10 ,0);
+//        Vector b = new Vector(20 ,0 ,0);
+//        
+//        a.printCoordinates();
+//        
+//        b.printCoordinates();
+//        
+//        double test = a.dot(b);
+//        System.out.println(test);
+//        
+//        Vector p = a.plus(b);
+//        p.printCoordinates();
+//        
+//        Vector m = b.minus(a);
+//        m.printCoordinates();
+//        
+//        Vector c = b.cross(a);
+//        c.printCoordinates();
+//        
+//        Vector rx = p.rotateByX(180);
+//        rx.printCoordinates();
+//        
+//        Vector ry = p.rotateByY(180);
+//        ry.printCoordinates();
+//        
+//        Vector rz = p.rotateByZ(90);
+//        rz.printCoordinates();
+//        
+//        Vector r = p.rotate(90, 90, 90);
+//        r.printCoordinates();
+//        for(int i=0; i<1; i++){
+//            System.out.println("Hello meh");
+//        }
+//        Vector v = new Vector(1, 1, 1);
+//            while(true){
+//                Scanner in = new Scanner(System.in);
+//                System.out.print("\nType equation: ");
+//                String str = in.nextLine();
+//            }
+    }
+}
